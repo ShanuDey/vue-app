79249fb6836f968a915a7833e52590bbb011b5fd
Shanu Dey <sd4shanudey@gmail.com>
2022-01-20 20:53:05 +0530

    added navigation drawer
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,32 @@
   <!-- App.vue -->
 
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
+    <v-navigation-drawer app v-model="drawer" :mini-variant.sync="mini">
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar
@@ -22,7 +46,7 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>Title</v-app-bar-title>
 
@@ -61,7 +85,13 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: true,
+    items: [
+      { title: 'Home', icon: 'mdi-home-city' },
+      { title: 'My Account', icon: 'mdi-account' },
+      { title: 'Users', icon: 'mdi-account-group-outline' },
+    ],
+    mini: true,
   }),
 };
 </script>
