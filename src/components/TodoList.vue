18fb62dcf48549578a660431e4767f0f1cb11588
Shanu Dey <sd4shanudey@gmail.com>
2022-01-21 21:59:45 +0530

    implemented delete todo
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-auto" max-width="500">
     <v-list>
       <v-list-item-group v-model="model">
-        <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item v-for="item in items" :key="item.id">
           <v-checkbox
             :input-value="item.completed"
             color="deep-purple accent-4"
@@ -11,7 +11,7 @@
             <v-list-item-title v-text="item.text"></v-list-item-title>
           </v-list-item-content>
           <v-list-item-icon>
-            <v-icon>mdi-delete</v-icon>
+            <v-icon @click="deleteTodo(item.id)">mdi-delete</v-icon>
           </v-list-item-icon>
         </v-list-item>
       </v-list-item-group>
@@ -24,13 +24,18 @@ export default {
   data() {
     return {
       items: [
-        { text: 'First Todo', completed: false },
-        { text: 'Second Todo', completed: false },
-        { text: 'Third Todo', completed: false },
-        { text: 'Fourth Todo', completed: false },
+        { id: 1, text: 'First Todo', completed: false },
+        { id: 2, text: 'Second Todo', completed: false },
+        { id: 3, text: 'Third Todo', completed: false },
+        { id: 4, text: 'Fourth Todo', completed: false },
       ],
       model: 1,
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.items = this.items.filter((todo) => todo.id !== id);
+    },
   },
 };
 </script>
