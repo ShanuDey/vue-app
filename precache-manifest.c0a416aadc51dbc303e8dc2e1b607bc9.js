ec45a01ba5ccc18177f734e1d7c9a1746c1ef8b6
Shanu Dey <sd4shanudey@gmail.com>
2022-01-21 23:57:32 +0530

    deploy

---
+++ precache-manifest.c0a416aadc51dbc303e8dc2e1b607bc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989b86c8b160417b4414",
+    "url": "/vue-app/css/app.9f968966.css"
+  },
+  {
+    "revision": "9fd254b2afd0c2191f8f",
+    "url": "/vue-app/css/chunk-vendors.9c61f04d.css"
+  },
+  {
+    "revision": "7416ae4fcb19e65bd56e",
+    "url": "/vue-app/css/todoList.86120c46.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0ac8c2bb1208a742672a178beea6ed70",
+    "url": "/vue-app/index.html"
+  },
+  {
+    "revision": "14b6d0181abb25bc4569",
+    "url": "/vue-app/js/about.3645fdd7.js"
+  },
+  {
+    "revision": "989b86c8b160417b4414",
+    "url": "/vue-app/js/app.54765079.js"
+  },
+  {
+    "revision": "9fd254b2afd0c2191f8f",
+    "url": "/vue-app/js/chunk-vendors.a7f9e99c.js"
+  },
+  {
+    "revision": "7416ae4fcb19e65bd56e",
+    "url": "/vue-app/js/todoList.b19cdcd5.js"
+  },
+  {
+    "revision": "a7bb829f05709f04c4cc8f0bb0289d89",
+    "url": "/vue-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app/robots.txt"
+  }
+]);
